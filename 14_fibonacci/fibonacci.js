fd6ccc11d6ce355f2213ed394b161d567a20b07f
Noureddine Boulefaa <bnoureddine300@gmail.com>
2025-11-06 21:34:36 +0100

    add 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n); 
+  if (n < 0) return "OOPS"; 
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= n; i++) {
+    const next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 
 };
 
