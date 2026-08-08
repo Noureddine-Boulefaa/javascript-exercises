829dbdf26896daaf60c34a8711da3ebeef35a8ea
Noureddine Boulefaa <bnoureddine300@gmail.com>
2026-08-08 22:18:18 +0100

    Solve recursion contains exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,21 @@
-const contains = function() {
-  
+const contains = function(object,toFind) {
+    
+    for(const key in object)
+    {
+        const val = object[key];
+        if(Object.is(val,toFind))
+        {
+            return true;
+        }
+        if(typeof val === "object"  && val !== null)
+        {
+            if(contains(val,toFind))
+            {
+                return true;
+            }
+        }
+    }
+  return false;
 };
   
 // Do not edit below this line
