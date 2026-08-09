756135252522371385cd09fedc5bb23252dff498
Noureddine Boulefaa <bnoureddine300@gmail.com>
2026-08-09 22:56:07 +0100

    Solve permutations recursion exercise

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,4 +1,24 @@
-const permutations = function() {
+const permutations = function(array) {
+    if(array.length <= 1)
+    {
+        return [array];
+    }
+    const result = [];
+    for(let i = 0;i < array.length; i++ )
+    {
+        const current = array[i];
+
+        const remaining = array.slice(0,i).concat(array.slice(i + 1));
+
+        const remainingPerms = permutations(remaining);
+
+        for(const perm of remainingPerms)
+        {
+            result.push([current,...perm]);
+        }
+
+    }
+    return result;
   
 };
   
