af9a523578d1ce1a8ea872b31100e72a09dfdfeb
Noureddine Boulefaa <bnoureddine300@gmail.com>
2026-08-09 20:28:48 +0100

    Solve totalIntegers exercise and pass all tests

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,4 +1,22 @@
-const totalIntegers = function() {
+const totalIntegers = function(object) {
+    if(typeof object !== "object" || object === null)
+    {
+        return undefined;
+    }
+    let count = 0;
+    for(const key in object)
+    {
+        const value = object[key];
+        if(Number.isInteger(value))
+        {
+            count++;
+        }
+        else if(typeof value === "object" && value !== null)
+        {
+            count+= totalIntegers(value);
+        }
+    }
+    return count;
   
 };
   
