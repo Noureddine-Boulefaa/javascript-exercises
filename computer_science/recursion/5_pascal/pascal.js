6c0fa6a0f3019d09ee9112494f1fa7c773a471ec
Noureddine Boulefaa <bnoureddine300@gmail.com>
2026-08-10 14:57:06 +0100

    Solve pascal recursion exercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,20 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+  if (n <= 1) {
+    return [1];
+  }
+
+
+  const prevRow = pascal(n - 1);
+  const currentRow = [1];
+
+ 
+  for (let i = 0; i < prevRow.length - 1; i++) {
+    currentRow.push(prevRow[i] + prevRow[i + 1]);
+  }
+
+  currentRow.push(1);
+
+  return currentRow;
 };
   
 // Do not edit below this line
